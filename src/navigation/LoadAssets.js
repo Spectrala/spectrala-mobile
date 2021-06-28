2fb2abf12ebdc6a597f90e8a0f0f5c7788f02341
Ian Fowler <ian@spidr.design>
2021-06-28 15:00:16 -0500

    Loader

--- src/navigation/LoadAssets.js
+++ src/navigation/LoadAssets.js
@@ -1,38 +1,44 @@
-import { Ionicons } from '@expo/vector-icons';
-import * as Font from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import * as React from 'react';
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import * as tf from "@tensorflow/tfjs";
+import React, { useEffect, useState } from "react";
+import { Camera } from "expo-camera";
 
-export default function useCachedResources() {
-  const [isLoadingComplete, setLoadingComplete] = React.useState(false);
+export default function Loader() {
+  const [tfReady, setTfReady] = useState(false);
+  const [hasCameraPermission, setHasCameraPermission] = useState(false);
+  const [localLoadingComplete, setLocalLoadingComplete] = useState(false);
+
+  const loadingIsComplete = () =>
+    tfReady && hasCameraPermission && localLoadingComplete;
+
+  const loadLocalFonts = async () => {
+    await Font.loadAsync({
+      Heebo: require("../../assets/fonts/Heebo/static/Heebo-Regular.ttf"),
+      "Heebo-SemiBold": {
+        uri: require("../../assets/fonts/Heebo/static/Heebo-SemiBold.ttf"),
+        display: Font.FontDisplay.FALLBACK,
+      },
+    });
+    setLocalLoadingComplete(true);
+  };
+
+  const loadTensorflow = async () => {
+    await tf.ready();
+    setTfReady(true);
+  };
+
+  const requestCameraPermission = async () => {
+    const { status } = await Camera.requestPermissionsAsync();
+    setHasCameraPermission(status === "granted");
+  };
 
   // Load any resources or data that we need prior to rendering the app
-  React.useEffect(() => {
-    async function loadResourcesAndDataAsync() {
-      try {
-        // TODO: Understand what commented splash screen lines are meant to do
-        // SplashScreen.preventAutoHideAsync();
-
-
-        await Font.loadAsync({
-          Heebo: require('../../assets/fonts/Heebo/static/Heebo-Regular.ttf'),
-          'Heebo-SemiBold': {
-            uri: require('../../assets/fonts/Heebo/static/Heebo-SemiBold.ttf'),
-            display: Font.FontDisplay.FALLBACK,
-          },
-        });
-      } catch (e) {
-        // We might want to provide this error information to an error reporting service
-        console.warn(e);
-      } finally {
-        setLoadingComplete(true);
-        // SplashScreen.hideAsync();
-      }
-    }
-
-    loadResourcesAndDataAsync();
+  useEffect(() => {
+    loadLocalFonts();
+    loadTensorflow();
+    requestCameraPermission();
   }, []);
 
-  return isLoadingComplete;
+  return loadingIsComplete();
 }
-
