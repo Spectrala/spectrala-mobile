03c998ca408b6b48bf9d20df506cadd8b0d1ea78
Ian Fowler <ian@spidr.design>
2021-07-06 10:16:59 -0500

    Tidy

--- src/screens/camera/CameraLoader.js
+++ src/screens/camera/CameraLoader.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import { Camera } from "expo-camera";
 import * as tf from "@tensorflow/tfjs";
 import PropTypes from "prop-types";
@@ -13,6 +13,7 @@ import {
   selectReaderLength,
   selectSecondCropBox,
 } from "../../redux/reducers/video";
+import * as jpeg from 'jpeg-js';
 
 export const CAMERA_VISIBILITY_OPTIONS = {
   full: "full",
@@ -59,6 +60,8 @@ export default function CameraLoader({ visibility, TensorCamera }) {
   const width = useSelector(selectReaderWidth);
   const length = useSelector(selectReaderLength);
 
+  const [previewImg, setPreviewImg] = useState(null);
+
   const requestCameraPermission = async () => {
     const { status } = await Camera.requestPermissionsAsync();
     setHasCameraPermission(status === "granted");
@@ -68,15 +71,58 @@ export default function CameraLoader({ visibility, TensorCamera }) {
     requestCameraPermission();
   }, []);
 
-  const handleTensor = (tensor) => {
-    // console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    console.log(tf.memory());
+  const getPaddedRange = (coordinates, idx, padding, limit) => {
+    const vals = coordinates.map((val) => val[idx]);
+    const min = Math.min(...vals);
+    const max = Math.max(...vals);
+    return {
+      min: Math.max(0, min - padding),
+      max: Math.min(limit || Infinity, max + padding),
+    };
+  };
+
+  const crudeSliceCrop = (img, coordinates, padding) => {
+    const { width, height } = { width: img.shape[1], height: img.shape[0] };
+    const yRng = getPaddedRange(coordinates, 0, padding, height);
+    const xRng = getPaddedRange(coordinates, 1, padding, width);
+    const sliceBegin = [yRng.min, xRng.min, 0];
+    const sliceSize = [yRng.max - yRng.min, xRng.max - xRng.min, 3];
+    return img.slice(sliceBegin, sliceSize);
+  };
+
+  async function tensorToImageUrl(imageTensor) {
+    const [height, width] = imageTensor.shape;
+
+    const buffer = await imageTensor.mul(255).toInt().data();
+    const frameData = new Uint8Array(width * height * 4);
+
+    let offset = 0;
+    for (let i = 0; i < frameData.length; i += 4) {
+      frameData[i] = buffer[offset];
+      frameData[i + 1] = buffer[offset + 1];
+      frameData[i + 2] = buffer[offset + 2];
+      frameData[i + 3] = 0xff;
+
+      offset += 3;
+    }
+
+    const rawImageData = {
+      data: frameData,
+      width,
+      height,
+    };
+    const jpegImageData = jpeg.encode(rawImageData, 75);
+    const base64Encoding = tf.util.decodeString(jpegImageData.data, "base64");
+    return base64Encoding;
+  }
+
+  const getRotatedFlaggedImage = (tensor) => {
+    // Convert from 0-255 to 0-1
     const img = tensor.mul(1 / 255);
 
     // Shape is [yIndex, xIndex, 3 for rgb]
     const imgHeight = tensor.shape[0];
     const imgWidth = tensor.shape[1];
-    tensor.dispose();
 
     // Array of indicies of corners of reader box
     const cornerIndicies = corners.map(({ x, y }) => [
@@ -89,38 +135,54 @@ export default function CameraLoader({ visibility, TensorCamera }) {
     const sparseVals = Array(sparseIndicies.length).fill(FLAG_PIXEL_OFFSET);
     const flags = tf.sparseToDense(sparseIndicies, sparseVals, tensor.shape);
     const imgFlagged = img.add(flags);
-    img.dispose();
 
     // Get a "crude" take on a cropped image, unaccounting for rotation
-    const yVals = cornerIndicies.map((vec) => vec[0]);
-    const xVals = cornerIndicies.map((vec) => vec[1]);
+    const imgCrude = crudeSliceCrop(imgFlagged, cornerIndicies, PADDING);
 
-    const xRng = { min: Math.min(...xVals), max: Math.max(...xVals) };
-    const yRng = { min: Math.min(...yVals), max: Math.max(...yVals) };
-    const xRngP = {
-      min: Math.max(0, xRng.min - PADDING),
-      max: Math.min(imgWidth, xRng.max + PADDING),
-    };
-    const yRngP = {
-      min: Math.max(0, yRng.min - PADDING),
-      max: Math.min(imgHeight, yRng.max + PADDING),
-    };
-    const sliceBegin = [yRngP.min, xRngP.min, 0];
-    const sliceSize = [yRngP.max - yRngP.min, xRngP.max - xRngP.min, 3];
-    const imgCrude = imgFlagged.slice(sliceBegin, sliceSize);
-    imgFlagged.dispose();
-    imgCrude.dispose();
-    // console.log(tf.memory());
+    // Pad the crude image. This allows enough room (in any case) for the rotation.
+    const crudeHeight = imgCrude.shape[0];
+    const crudeWidth = imgCrude.shape[1];
+    const side = Math.max(crudeHeight, crudeWidth);
+    const getPad = (x) => Math.round(Math.max(side - x, 0) / 2) + PADDING;
+    const padHoriz = Array(2).fill(getPad(crudeWidth));
+    const padVert = Array(2).fill(getPad(crudeHeight));
+    // Pad and make tensor 4d because rotateWithOffset requires 4d.
+    const imgPad = tf.pad(imgCrude, [padVert, padHoriz, [0, 0]]).expandDims();
+
+    // Rotate the padded image such that the reader box is vertical 
+    // (axis with the dots going up and down)
+    const rotateRadians = (boxAngle * Math.PI) / 180;
+    const imgRotated = tf.image.rotateWithOffset(imgPad, rotateRadians);
+
+    // Reduce to 3 dimensions for rest of operations
+    const imgRotated3d = imgRotated.squeeze();
+
+    return imgRotated3d;
+  };
+
+  const finishIt = async (imgRotated) => {
+    // Find the flags previously put in the image and crop around them
+    const maskFlags = imgRotated.less([0]).asType("bool");
+    const flagCoordinates = await tf.whereAsync(maskFlags);
+    const flagCoordArr = await flagCoordinates.slice([0, 1], [-1, 2]).array();
+    const shapedImg = crudeSliceCrop(imgRotated, flagCoordArr, 0);
+    const url = await tensorToImageUrl(shapedImg);
+    console.log("data:image/jpeg;base64," + url);
+    setPreviewImg(`data:image/jpeg;base64,${url}`);
   };
 
   const handleCameraStream = (images, updatePreview, gl) => {
     const loop = async () => {
-      tf.tidy(() => {
+      const rotatedImg = tf.tidy(() => {
         const nextImageTensor = images.next().value;
         if (nextImageTensor) {
-          handleTensor(nextImageTensor);
+          return getRotatedFlaggedImage(nextImageTensor);
         }
+        return null;
       });
+      if (rotatedImg) {
+        finishIt(rotatedImg);
+      }
       requestAnimationFrame(loop);
     };
     loop();
@@ -129,20 +191,31 @@ export default function CameraLoader({ visibility, TensorCamera }) {
   // console.log(dimensions);
 
   return (
-    hasCameraPermission && (
-      <TensorCamera
-        style={styles.camera}
-        type={cameraType}
-        zoom={0}
-        cameraTextureHeight={dimensions.height}
-        cameraTextureWidth={dimensions.width}
-        resizeHeight={dimensions.height}
-        resizeWidth={dimensions.width}
-        resizeDepth={3}
-        onReady={handleCameraStream}
-        autorender={true}
-      />
-    )
+    <>
+      {previewImg && (
+        <Image
+          style={styles.image}
+          source={{
+            uri: previewImg,
+          }}
+          resizeMode="contain"
+        />
+      )}
+      {hasCameraPermission && (
+        <TensorCamera
+          style={styles.camera}
+          type={cameraType}
+          zoom={0}
+          cameraTextureHeight={dimensions.height}
+          cameraTextureWidth={dimensions.width}
+          resizeHeight={dimensions.height}
+          resizeWidth={dimensions.width}
+          resizeDepth={3}
+          onReady={handleCameraStream}
+          autorender={true}
+        />
+      )}
+    </>
   );
 }
 
@@ -157,5 +230,12 @@ const styles = StyleSheet.create({
     // top: 0,
     width: "100%",
     aspectRatio: dimensions.width / dimensions.height,
+  },
+  image: {
+    paddingHorizontal: 10,
+    borderWidth: 2,
+    borderColor: "black",
+    width: "100%",
+    height: 100,
   },
 });
